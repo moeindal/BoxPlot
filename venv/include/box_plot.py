e851130840b699b3532e183f7253192e2b93e480
Moein Dal <m.dehshirii.p@gmail.com>
2019-03-11 20:26:32 +0330

    A little Change !

--- venv/include/box_plot.py
+++ venv/include/box_plot.py
@@ -1,21 +1,24 @@
 import csv
 import matplotlib.pyplot as plt
 
-
 def create_box_plot():
     with open("allCameras_editor_review.tsv") as tsvFile:
         tsv_reader = csv.reader(tsvFile, delimiter="\t")
-        temp_array = []
-        l = -1
-        for line in tsv_reader:
-            if l == 0:
-                l = l + 1
-                continue
-            if line[1][15:18] != '':
-                temp_array.append(float(line[1][15:18].replace(',', '')))
-            l = l + 1
 
-        plt.boxplot(temp_array)
+        temp_list = []
+        counter = -1
+        for line in tsv_reader:
+            #First Line In tsv_reader Is Titles So We Must Ignore This Line And Use Other Lines For Create Box Plot
+            if counter == 0:
+                counter = counter + 1
+                continue
+
+            if line[1][15:18] != '':
+                # Every Rating Filed Has Some Text And A Rate Number, We Just Need The Number
+                temp_list.append(float(line[1][15:18]))
+            counter = counter + 1
+
+        plt.boxplot(temp_list)
         plt.show()
 
 
